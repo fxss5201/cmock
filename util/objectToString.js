21fcc3a3616ec2cac0c7f5e1aa592e5d59f2f0de
fanyushun <fanyushun@zj.tech>
2022-02-17 16:10:21 +0800

    feat: 调整生成 body 中 key 的规则

--- util/objectToString.js
+++ util/objectToString.js
@@ -1,16 +1,22 @@
+const replaceAll = require("./replaceAll.js");
 /**
  * 将对象转换成字符串
  * @param {object} object 需要被转换的对象
  * @param {array} keys 保留的key
  * @returns {string}
- * 例如 objectToString({ name: 'fxss', age: 28, location: 'shanghai' }, ['name', 'age']) = namefxssage20
+ * 例如 objectToString({ name: 'fxss', age: 28, location: 'shanghai' }, ['name', 'age']) = name_fxss___age_28
  */
 module.exports = function (object, keys = []) {
-  let res = "";
+  let res = [];
   Object.keys(object).forEach((item) => {
     if (keys.includes(item)) {
-      res += `${item}${object[item]}`;
+      let value = object[item];
+      if (Object.prototype.toString.call(value).slice(8, -1) === "String") {
+        value = replaceAll(".", "-", value);
+        value = replaceAll("/", "-", value);
+      }
+      res.push(`${item}_${value}`);
     }
   });
-  return res;
+  return res.join("___");
 };
